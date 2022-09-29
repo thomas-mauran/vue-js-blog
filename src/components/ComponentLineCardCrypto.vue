7500f3bb19262f15d19afeb4bc7c3bdcdec744cb
Thomas Mauran <78204354+thomas-mauran@users.noreply.github.com>
2022-09-29 19:21:13 +0200

    cryptotracker p1

---
+++ src/components/ComponentLineCardCrypto.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="lineCard">
+        <img class="logo" :src="image" :alt="name" />
+        <h1>{{ name }}</h1>
+        <h3>{{ symbol }}</h3>
+        <h3>{{ current_price }} $</h3>
+        <h4 class="marketCap">Market cap : {{ market_cap }} $</h4>
+        <h4 class="high_24h">High 24h : {{ high_24h }} $</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ComponentLineCardCrypto',
+    props: {
+        symbol: String,
+        name: String,
+        id: Int16Array,
+        image: String,
+        current_price: Int16Array,
+        market_cap: Int16Array,
+        high_24h: Int16Array,
+    },
+};
+</script>
+<style>
+.lineCard {
+    margin-top: 30px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    box-shadow: 3px 3px 10px 1px black;
+    width: 90%;
+    border-radius: 15px;
+}
+
+.lineCard h1 {
+    padding: 20px;
+}
+
+.lineCard h3 {
+    margin: 20px;
+}
+
+.lineCard h4 {
+    margin: 20px;
+}
+
+.logo {
+    margin: 2%;
+    width: 50px;
+}
+
+.marketCap {
+    color: rgb(201, 168, 79);
+}
+
+.high_24h {
+    color: green;
+}
+</style>
--- src/views/CryptoTrackerVue.vue
+++ src/views/CryptoTrackerVue.vue
@@ -1,35 +1,46 @@
 <template>
-    <h1>Hello world</h1>
-    <div class="listContainer" :v-for="items in list" :key="dataSet.id">
-        <h1>{{ items.id }}</h1>
+    <h1>Voici les 100 cryptos les plus importantes du marché 🚀 !</h1>
+    <div class="listContainer" v-for="items in dataSet" :key="items.id">
+        <ComponentLineCardCrypto
+            :name="items.name"
+            :id="items.id"
+            :symbol="items.symbol"
+            :image="items.image"
+            :current_price="items.current_price"
+            :market_cap="items.market_cap"
+            :high_24h="items.high_24h"
+        />
     </div>
 </template>
 <script>
+import ComponentLineCardCrypto from '@/components/ComponentLineCardCrypto.vue';
 export default {
     name: 'CryptoTrackerVue',
-
     data() {
         return {
-            dataSet: {},
+            dataSet: [],
         };
     },
     methods: {
         retrieveData() {
-            let xml = new XMLHttpRequest();
-            xml.open('GET', 'https://api.coingecko.com/api/v3/coins/list');
-            xml.onload = () => {
-                if (xml.status != 200) {
-                    console.log(`Error : ${xml.statusText}`);
-                } else {
-                    console.log(xml.responseText);
-                }
-            };
-            xml.send();
+            // Simple GET request using fetch
+            fetch(
+                'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc'
+            )
+                .then((response) => response.json())
+                .then((data) => (this.dataSet = data))
+                .then(console.log(this.dataSet));
         },
     },
-    beforeMount() {
+    mounted() {
         this.retrieveData();
     },
+    components: { ComponentLineCardCrypto },
 };
 </script>
-<style></style>
+<style>
+.listContainer {
+    display: flex;
+    justify-content: center;
+}
+</style>
